c1b32f6fa897651d0047a2b249dcc9531b73d464
Malcolm Reid <malcolm.reid@me.com>
2018-05-25 15:33:29 -0700

    Initial commit
    skeleton of app - working on user login via sqlplus

---
+++ GardenMaster/Config/Database_DDL.sql
@@ -0,0 +1,2 @@
+-- keep a script for DB setup:
+create database gardenmaster;
---
+++ GardenMaster/src/control/UserCreator.java
@@ -0,0 +1,16 @@
+package control;
+
+import javax.servlet.http.HttpServlet;
+
+public class UserCreator extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UserCreator() {
+		
+	}
+
+}
